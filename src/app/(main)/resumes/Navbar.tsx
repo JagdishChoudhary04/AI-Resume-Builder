b4522887bdc852e3d2af5d1ec72af612721fd745
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2025-09-25 21:39:13 +0530

    Navbar and Theme Added

--- src/app/(main)/resumes/Navbar.tsx
+++ src/app/(main)/resumes/Navbar.tsx
@@ -1,9 +1,39 @@
+import Image from "next/image"
+import Link from "next/link";
+import logo from "@/assests/logo.jpg"
+import { UserButton } from "@clerk/nextjs";
+import ThemeToggle from "@/components/ThemeToggle";
 export default function Navbar(){
     return (
         <header className="shadow-sm">
-            <div className="mx-auto flex max-w-7xl items-center justify-between gap-3"></div>
-            
-            
+            <div className="mx-auto flex max-w-7xl items-center justify-between gap-3">
+                
+                <Link  href="/resumes" className="flex items-center gap-2">
+                    <Image 
+                        src = {logo}
+                        alt="Logo"
+                        width = {55}
+                        height = {55}
+                        className="m-2 rounded-full"
+                    />
+                    <span className="text-xl font-bold tracking-tight">
+                        AI Resume Builder
+                    </span>
+                </Link>
+                <div className="flex items-center gap-3">
+                    <ThemeToggle/>
+                    <UserButton
+                        appearance={{
+                            elements: {
+                                avatarBox: {
+                                    width: 45,
+                                    height: 45,
+                                },
+                            },
+                        }}
+                    />
+                </div>
+            </div>
         </header>
     );
 }
